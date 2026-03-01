afa776adb5def10a9cb32b134f3a43543dc37943
shova10 <shovap33@gmail.com>
2026-03-01 20:40:06 +0545

    Day 1: Project setup — Vite, React, Tailwind, Router, Axios, RHF, Toast, Lucide

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Toaster } from 'react-hot-toast'
+
+const App = () => {
+  return (
+    <div>
+      <Toaster position="top-right" />
+    </div>
+  )
+}
+
+export default App
