ee0ae44048212f352e42b3208c10b93d0e471164
Binita <bbhusal394@gmail.com>
2026-03-10 22:08:51 +0545

    feat: add complaints serializers and citizen views

---
+++ backend/complaints/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Complaint, StatusHistory
+
+class ComplaintCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Complaint
+        fields = ['title', 'description', 'image', 'category', 'location_name', 'location_lat', 'location_lng', 'language']
+        
+class ComplaintListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Complaint
+        fields = ['title', 'id', 'category', 'status', 'priority', 'created_at', 'upvote_count']
+
+class StatusHistorySerializer(serializers.ModelSerializer):
+    changed_by = serializers.StringRelatedField()
+    class Meta:
+        model = StatusHistory
+        fields = ['previous_status', 'new_status', 'changed_by', 'changed_at', 'remark']
+
+class ComplaintDetailSerializer(serializers.ModelSerializer):
+    status_history = StatusHistorySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Complaint
+        fields = ['id', 'title', 'description', 'image', 'category', 'priority', 'status',  'location_name', 'location_lat', 'location_lng', 'language', 'ai_category', 'ai_priority', 'ai_confidence', 'is_duplicate', 'upvote_count', 'created_at', 'updated_at', 'resolved_at', 'status_history']
--- backend/complaints/views.py
+++ backend/complaints/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from .models import Complaint, StatusHistory, ComplaintUpvote
+from .serializers import ComplaintCreateSerializer, ComplaintListSerializer, ComplaintDetailSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import PermissionDenied
 
-# Create your views here.
+
+class ComplaintListCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return ComplaintCreateSerializer
+        return ComplaintListSerializer
+
+    def get_queryset(self):
+        return Complaint.objects.filter(citizen=self.request.user)
+
+class ComplaintDetailVIew(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ComplaintDetailSerializer
+
+    def get_object(self):
+        complaint_id = self.kwargs['pk']
+        complaint = get_object_or_404(Complaint, id=complaint_id)
+
+        if complaint.citizen != self.request.user:
+            raise PermissionDenied("You cannot access this complaint.")
+
+        return complaint
+
+class UpvoteToggleView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        complaint = get_object_or_404(Complaint, id=pk)
+
+        upvote = ComplaintUpvote.objects.filter(
+            citizen=request.user, 
+            complaint=complaint
+            ).first()
+        
+        if upvote:
+            upvote.delete()
+            complaint.upvote_count -= 1
+            complaint.save()
+            return Response({ "message": "Upvote removed", "upvote_count": complaint.upvote_count }, status=status.HTTP_200_OK)
+
+        ComplaintUpvote.objects.create(
+            citizen=request.user,
+            complaint=complaint
+        ) 
+        complaint.upvote_count += 1 
+        complaint.save() 
+        return Response({ "message": "Upvoted successfully", "upvote_count": complaint.upvote_count }, status=status.HTTP_201_CREATED)
