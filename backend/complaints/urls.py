a116a23c223bdb325df6d96b5f307cc2d9e073af
Binita <bbhusal394@gmail.com>
2026-04-24 23:13:36 +0545

    fix: admin summary and trend viewa

--- backend/complaints/urls.py
+++ backend/complaints/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ComplaintListCreateView, ComplaintDetailView, UpvoteToggleView, AdminComplaintListView, StatusUpdateView
+from .views import ComplaintListCreateView, ComplaintDetailView, UpvoteToggleView, AdminComplaintListView, StatusUpdateView, StaffIssueListView, StaffStatusUpdateView, HeatmapView, AdminSummaryView, BulkUpdateView, AdminTrendsView
 
 urlpatterns = [
     path('complaints/', ComplaintListCreateView.as_view(), name ='complaint-list'),
@@ -7,4 +7,11 @@ urlpatterns = [
     path('complaints/<int:pk>/upvote/', UpvoteToggleView.as_view(), name='upvote-toggle'),
     path('admin/complaints/', AdminComplaintListView.as_view(), name='admin-complaint-list'),
     path('admin/complaints/<int:pk>/status/', StatusUpdateView.as_view(), name='status-update'),
+    path('staff/issues/', StaffIssueListView.as_view(), name='staff-issues'),
+    path('staff/issues/<int:pk>/status/', StaffStatusUpdateView.as_view(), name='staff-status-update'),
+    path('issues/heatmap/', HeatmapView.as_view(), name='heatmap'),
+    path('admin/summary/', AdminSummaryView.as_view(), name='admin-summary'),
+    path('admin/issues/bulk-update/', BulkUpdateView.as_view(), name='bulk-update'),
+    path('admin/summary', AdminSummaryView.as_view(), name='admin-summary'),
+    path('admin/trends/', AdminTrendsView.as_view(), name='admin-trends'),
 ]
